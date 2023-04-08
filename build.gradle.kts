47fcc2898cbffc1b03617ca8466cb436c10d49ef
defaultzon3 <98385732+defaultzon3@users.noreply.github.com>
2023-04-08 23:10:31 +0300

    Connected `shadowJar` plugin in `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
     application
 }
 
@@ -28,4 +29,11 @@ kotlin {
 
 application {
     mainClass.set("MainKt")
+}
+
+shadow {
+    dependencies {
+        implementation("org.yaml:snakeyaml:2.0")
+        implementation("org.json:json:20230227")
+    }
 }
